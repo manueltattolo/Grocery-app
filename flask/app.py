9f7d61c1b5e0d8ec9b3e8dce04190ccd71e06851
manueltattolo <emanuele.tattolo@datadoghq.com>
2025-03-25 09:51:16 +0000

    Fix flake8 issues

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, current_app
+from flask import Flask, render_template, request, redirect, url_for
 from flask.logging import default_handler
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,16 +8,23 @@ import structlog
 app = Flask(__name__)
 app.logger.removeHandler(default_handler)
 app.logger.handlers.clear()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@postgres-service:5432/grocery_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'postgresql://postgres:admin@postgres-service:5432/grocery_db'
+    )
 db = SQLAlchemy(app)
 
-structlog.configure(processors=[structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=True), structlog.processors.JSONRenderer()])
+structlog.configure(processors=[
+    structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=True),
+    structlog.processors.JSONRenderer()
+    ])
 log = structlog.get_logger()
+
 
 class Grocery(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     items = db.Column(db.String(200), nullable=False)
+
 
 def create_app():
     with app.app_context():
@@ -31,7 +38,8 @@ def create_app():
                     raise e
                 retries -= 1
                 time.sleep(1)
-                
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -44,10 +52,16 @@ def index():
         return redirect(url_for('success', name=name, items=items))
     return render_template('index.html')
 
+
 @app.route('/success/<string:name>/<string:items>')
 def success(name, items):
-    log.info("All items added successfully.", user=name, list=items)
+    log.info(
+        "All items added successfully.",
+        user=name,
+        list=items
+        )
     return render_template('success.html')
+
 
 @app.route('/lists')
 def lists():
@@ -55,12 +69,19 @@ def lists():
         return redirect(url_for('delete_all_items'))
     try:
         groceries = Grocery.query.all()
-        groceries_json = [{'user': grocery.name, 'list': grocery.items} for grocery in groceries]
+        groceries_json = [{
+            'user': grocery.name,
+            'list': grocery.items
+            } for grocery in groceries]
         log.info("Retrieved groceries", groceries=groceries_json)
-        return render_template('lists.html', groceries=groceries)
+        return render_template(
+            'lists.html',
+            groceries=groceries
+            )
     except Exception as e:
         log.error("Error retrieving groceries: %s", str(e))
         return "An error occurred while retrieving groceries.", 500
+
 
 @app.route('/delete-all-items', methods=['POST'])
 def delete_all_items():
@@ -69,9 +90,11 @@ def delete_all_items():
         db.session.commit()
         log.info("All items deleted successfully.")
         return redirect(url_for('lists'))
+
     except Exception as e:
         log.error("Error deleting items: %s", str(e))
         return "An error occurred while deleting items.", 500
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
